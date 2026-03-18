065e2370ceccb5874e733d9519e3e33334637167
Sutus László <h051618@stud.u-szeged.hu>
2026-03-18 12:49:10 +0100

    initialize

---
+++ src/main/java/WorkflowManager/WorkflowManagerApplication.java
@@ -0,0 +1,13 @@
+package WorkflowManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WorkflowManager/WorkflowManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package WorkflowManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
