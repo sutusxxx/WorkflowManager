391d3e7dc17c15d9717445fc95df85526a0f2d8b
Sutus László <h051618@stud.u-szeged.hu>
2026-03-20 06:35:21 +0100

    Add custom exception handling.

--- src/main/java/WorkflowManager/task/TaskController.java
+++ src/main/java/WorkflowManager/task/TaskController.java
@@ -1,14 +1,18 @@
 package WorkflowManager.task;
 
+import WorkflowManager.exceptions.TaskNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/task")
 public class TaskController {
     private final TaskService taskService;
 
@@ -17,8 +21,17 @@ public class TaskController {
         this.taskService = taskService;
     }
 
-    @GetMapping("/tasks")
+    @GetMapping("/all")
     public List<Task> getAllTasks() {
         return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getById(@PathVariable Long id) {
+        try {
+            return taskService.getTaskById(id);
+        } catch (TaskNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
     }
 }
--- src/main/java/WorkflowManager/task/TaskService.java
+++ src/main/java/WorkflowManager/task/TaskService.java
@@ -1,5 +1,6 @@
 package WorkflowManager.task;
 
+import WorkflowManager.exceptions.TaskNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,9 @@ public class TaskService {
 
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
+    }
+
+    public Task getTaskById(Long id) throws TaskNotFoundException {
+        return taskRepository.findById(id).orElseThrow(TaskNotFoundException::new);
     }
 }
