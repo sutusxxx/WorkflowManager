489252ec7d57082269af3f319c7758151b8d1a68
sutusxxx <sutus.laszlo96@gmail.com>
2026-04-22 11:56:25 +0200

    Create custom query to get issues for project-

--- src/main/java/WorkflowManager/issue/repository/IssueRepository.java
+++ src/main/java/WorkflowManager/issue/repository/IssueRepository.java
@@ -2,6 +2,7 @@ package WorkflowManager.issue.repository;
 
 import WorkflowManager.issue.Issue;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.Collection;
 import java.util.List;
@@ -9,6 +10,8 @@ import java.util.Optional;
 
 public interface IssueRepository extends MongoRepository<Issue, String> {
     Optional<Issue> findByKey(String key);
+
+    @Query("{ 'projectId': ?0, 'type': { $ne: 'SUBTASK' } }")
     List<Issue> findByProjectId(String projectId);
     List<Issue> findByParentId(String parentId);
     List<Issue> findByParentIdIn(Collection<String> parentIds);
